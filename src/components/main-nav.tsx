f966d174ea6e06d4ae3dd4440eb51c42a0b81c23
Mamaly1000 <105161078+Mamaly1000@users.noreply.github.com>
2024-04-13 01:58:48 +0330

    last v

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -67,7 +67,7 @@ export function MainNav({
           className={cn(
             "text-sm font-medium transition-colors hover:text-primary",
             route.active
-              ? "text-black dark:text-white"
+              ? "text-black  dark:text-white"
               : "text-muted-foreground"
           )}
         >
